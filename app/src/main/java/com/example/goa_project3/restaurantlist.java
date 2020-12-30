916a49f050b7df4d7b0049ef9e261c174c6872ef
zjayee <75448434+zjayee@users.noreply.github.com>
2020-12-29 23:28:28 -0800

    fragment

--- app/src/main/java/com/example/goa_project3/restaurantlist.java
+++ app/src/main/java/com/example/goa_project3/restaurantlist.java
@@ -8,6 +8,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link restaurantlist#newInstance} factory method to
@@ -17,12 +19,9 @@ public class restaurantlist extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String HEADING = "heading";
+    private static final String RESTAURANTS = "restaurants";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public restaurantlist() {
         // Required empty public constructor
@@ -32,16 +31,16 @@ public class restaurantlist extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
+     * @param heading title of list
+     * @param restaurants contents of list
      * @return A new instance of fragment restaurantlist.
      */
     // TODO: Rename and change types and number of parameters
-    public static restaurantlist newInstance(String param1, String param2) {
+    public static restaurantlist newInstance(String heading, ArrayList<Restaurant> restaurants) {
         restaurantlist fragment = new restaurantlist();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString(HEADING, heading);
+        args.putSerializable(RESTAURANTS, restaurants);
         fragment.setArguments(args);
         return fragment;
     }
@@ -50,8 +49,8 @@ public class restaurantlist extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            String heading = getArguments().getString(HEADING);
+            ArrayList<Restaurant> restaurants = (ArrayList<Restaurant>) getArguments().getSerializable(RESTAURANTS);
         }
     }
 
