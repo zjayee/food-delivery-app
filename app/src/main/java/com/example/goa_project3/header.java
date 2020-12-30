af7b0af0e55b07049ca5962c9e358d2e516a7833
zjayee <75448434+zjayee@users.noreply.github.com>
2020-12-29 20:27:10 -0800

    added header fragment to main activity

--- app/src/main/java/com/example/goa_project3/header.java
+++ app/src/main/java/com/example/goa_project3/header.java
@@ -1,14 +1,21 @@
 package com.example.goa_project3;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Location;
 import android.os.Bundle;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.SearchView;
+import android.widget.TextView;
+
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,52 +24,32 @@ import android.widget.SearchView;
  */
 public class header extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public header() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment header.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static header newInstance(String param1, String param2) {
+
+    public static header newInstance() {
         header fragment = new header();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_header, container, false);
-
-
+        rootView = inflater.inflate(R.layout.fragment_header, container, false);
+        setupProperties();
+        return rootView;
     }
+
+    View rootView;
+    String location = "Vancouver";
+
+    private void setupProperties(){
+        TextView locationtext = rootView.findViewById(R.id.locationtextview);
+        locationtext.setText(location);
+    }
+
 }
