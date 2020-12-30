77d01b1c7dbcb4ea13829fec871edfa3a5a2d258
zjayee <75448434+zjayee@users.noreply.github.com>
2020-12-29 20:58:22 -0800

    dishes class, restaurantlist

---
+++ app/src/main/java/com/example/goa_project3/Dish.java
@@ -0,0 +1,11 @@
+package com.example.goa_project3;
+
+public class Dish {
+    private String name;
+    private String description;
+    private Integer image;
+    private Integer price;
+    private Integer rating;
+
+    
+}
