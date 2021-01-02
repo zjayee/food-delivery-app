c1ee84ff5da82cb6ecaa5acd6be6b266a8379dff
zjayee <75448434+zjayee@users.noreply.github.com>
2021-01-01 22:21:42 -0800

    category fragment set views

--- app/src/main/java/com/example/goa_project3/categorylist.java
+++ app/src/main/java/com/example/goa_project3/categorylist.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -44,9 +46,54 @@ public class categorylist extends Fragment {
     }
 
     private View rootView;
+    private View veganCard;
+    private View asianCard;
+    private View westernCard;
+    private View mexicanCard;
+    private View fastFoodCard;
+    private View greekCard;
 
     private void setProperties(){
-        
+    //find layout views with ids
+        veganCard = rootView.findViewById(R.id.vegan);
+        asianCard = rootView.findViewById(R.id.asian);
+        westernCard = rootView.findViewById(R.id.western);
+        mexicanCard = rootView.findViewById(R.id.mexican);
+        fastFoodCard = rootView.findViewById(R.id.fastfood);
+        greekCard = rootView.findViewById(R.id.greek);
+
+     //find images
+        ImageView veganicon = veganCard.findViewById(R.id.categoryimage);
+        ImageView asianicon = asianCard.findViewById(R.id.categoryimage);
+        ImageView westernicon = westernCard.findViewById(R.id.categoryimage);
+        ImageView mexicanicon = mexicanCard.findViewById(R.id.categoryimage);
+        ImageView fastfoodicon = fastFoodCard.findViewById(R.id.categoryimage);
+        ImageView greekicon = greekCard.findViewById(R.id.categoryimage);
+
+    //set images
+        veganicon.setImageResource(R.drawable.vegan);
+        asianicon.setImageResource(R.drawable.asian);
+        westernicon.setImageResource(R.drawable.western);
+        mexicanicon.setImageResource(R.drawable.mexican);
+        fastfoodicon.setImageResource(R.drawable.fastfood);
+        greekicon.setImageResource(R.drawable.greek);
+
+    //find text
+        TextView vegantext = veganCard.findViewById(R.id.categoryname);
+        TextView asiantext = asianCard.findViewById(R.id.categoryname);
+        TextView westerntext = westernCard.findViewById(R.id.categoryname);
+        TextView mexicantext = mexicanCard.findViewById(R.id.categoryname);
+        TextView fastfoodtext = fastFoodCard.findViewById(R.id.categoryname);
+        TextView greektext = greekCard.findViewById(R.id.categoryname);
+
+    //set text
+        vegantext.setText("Vegan");
+        asiantext.setText("Asian");
+        westerntext.setText("Western");
+        mexicantext.setText("Mexican");
+        fastfoodtext.setText("Fast Food");
+        greektext.setText("Greek");
+
     }
 
     private void setButtons(){
