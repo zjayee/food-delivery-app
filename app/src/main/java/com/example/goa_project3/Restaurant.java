be60fd28eaae0dbb46b940e4fde8b761ce6c763a
zjayee <75448434+zjayee@users.noreply.github.com>
2021-01-02 23:50:54 -0800

    set up restaurant list recyclerview

--- app/src/main/java/com/example/goa_project3/Restaurant.java
+++ app/src/main/java/com/example/goa_project3/Restaurant.java
@@ -7,9 +7,9 @@ public class Restaurant {
     private String cuisine;
     private Integer image;
     private ArrayList<Category> categories;
-    private Double rating;
+    private float rating;
 
-    public Restaurant(String name, String cuisine, Integer image, ArrayList<Category> categories, Double rating ){
+    public Restaurant(String name, String cuisine, Integer image, ArrayList<Category> categories, float rating ){
         this.name = name;
         this.cuisine = cuisine;
         this.image = image;
@@ -33,7 +33,7 @@ public class Restaurant {
         return categories;
     }
 
-    public Double getRating() {
+    public float getRating() {
         return rating;
     }
 }
--- app/src/main/java/com/example/goa_project3/restaurantlist.java
+++ app/src/main/java/com/example/goa_project3/restaurantlist.java
@@ -1,12 +1,22 @@
 package com.example.goa_project3;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -49,15 +59,121 @@ public class restaurantlist extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            String heading = getArguments().getString(HEADING);
-            ArrayList<Restaurant> restaurants = (ArrayList<Restaurant>) getArguments().getSerializable(RESTAURANTS);
+            heading = getArguments().getString(HEADING);
+            restaurants = (ArrayList<Restaurant>) getArguments().getSerializable(RESTAURANTS);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_restaurantlist, container, false);
+        rootView = inflater.inflate(R.layout.fragment_restaurantlist, container, false);
+        setupHeading();
+        setupRecyclerView();
+        return rootView;
     }
+
+    private String heading;
+    private ArrayList<Restaurant> restaurants;
+    private View rootView;
+    private RecyclerView recyclerView;
+
+    private void setupHeading(){
+        TextView headingtextview = rootView.findViewById(R.id.labeltextview);
+        headingtextview.setText(heading);
+
+    }
+
+    private void  setupRecyclerView(){
+        recyclerView = rootView.findViewById(R.id.restaurantrecyclerview);
+        //adjusts height
+        ViewGroup.LayoutParams params = recyclerView.getLayoutParams();
+        switch (heading)
+        {
+            case "Recommended":
+                params.height = 203;
+                break;
+            case "Recents":
+                params.height = 167;
+                break;
+        }
+
+        //create linear layout manager for recycler view
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layoutManager);
+
+        //set adaptor
+        RestaurantAdapter restaurantAdapter = new RestaurantAdapter(getContext(), restaurants);
+        recyclerView.setAdapter(restaurantAdapter);
+
+    }
+
+    private void userTappedOnPosition(int position){
+        //TODO: setup this
+        //video time instructions: 28min
+
+    }
+
+    private class RestaurantAdapter extends RecyclerView.Adapter<RestaurantViewHolder>{
+        Context context;
+        ArrayList<Restaurant> restaurants;
+
+        RestaurantAdapter(Context context, ArrayList<Restaurant> restaurants){
+            this.context = context;
+            this.restaurants = restaurants;
+        }
+        @NonNull
+        @Override
+        public RestaurantViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(context);
+            View itemView = inflater.inflate(R.layout.restaurant_card, parent, false);
+            RestaurantViewHolder viewHolder = new RestaurantViewHolder(itemView);
+            return viewHolder;
+
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull RestaurantViewHolder holder, int position) {
+            Restaurant restaurant = restaurants.get(position);
+            holder.restaurantName.setText(restaurant.getName());
+            holder.restaurantCategory.setText(restaurant.getCuisine());
+            holder.ratingBar.setRating(restaurant.getRating());
+            holder.restaurantImage.setImageResource(restaurant.getImage());
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                   userTappedOnPosition(position);
+                }
+            });
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
+    }
+
+    private class RestaurantViewHolder extends RecyclerView.ViewHolder{
+
+        public View itemView;
+        public ImageView restaurantImage;
+        public TextView restaurantName;
+        public TextView restaurantCategory;
+        public RatingBar ratingBar;
+
+        public RestaurantViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.itemView = itemView;
+            restaurantImage = itemView.findViewById(R.id.restaurantimage);
+            restaurantName = itemView.findViewById(R.id.restaurantname);
+            restaurantCategory = itemView.findViewById(R.id.ratingBar);
+
+        }
+    }
+
+
+
 }
